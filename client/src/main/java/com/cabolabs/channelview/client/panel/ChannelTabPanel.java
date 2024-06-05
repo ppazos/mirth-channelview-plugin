5e5feb826cc0da3c30f7f69005db1244cf7ee499
Pablo Pazos <pablo.pazos@cabolabs.com>
2024-06-05 18:41:03 -0300

    improvements for the UI

--- client/src/main/java/com/cabolabs/channelview/client/panel/ChannelTabPanel.java
+++ client/src/main/java/com/cabolabs/channelview/client/panel/ChannelTabPanel.java
@@ -36,6 +36,7 @@ import org.jgrapht.graph.DefaultListenableGraph;
 import java.awt.*;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import com.mirth.connect.model.Channel;
 import com.mirth.connect.model.Connector;
@@ -52,9 +53,9 @@ public class ChannelTabPanel extends AbstractChannelTabPanel { // extends JPanel
    // private JLabel forcefulLabel;
    // private MirthCheckBox forcefulCheckbox;
 
-   private JLabel channelLabel;
-   private MirthTextArea channelTextArea;
-   
+   //private JLabel channelLabel;
+   //private MirthTextArea channelTextArea;
+
    // To avoid the text in the arrows
    class MyEdge extends DefaultEdge {
 	    @Override
@@ -76,10 +77,11 @@ public class ChannelTabPanel extends AbstractChannelTabPanel { // extends JPanel
    }
 
    private void initComponents() {
-      setBackground(Color.WHITE);
+      setBackground(Color.ORANGE);
       setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
       //setLayout(new MigLayout("insets 12, novisualpadding, hidemode 3, fill, gap 6", "[]", "[]")); // layout, column, row
- 
+
 
       /**
       * Instantiate component variables
@@ -102,6 +104,9 @@ public class ChannelTabPanel extends AbstractChannelTabPanel { // extends JPanel
       /**
       * Create the base container for our components
       */
+
+
+      /* <<<<<<<<<<<<<<
       JPanel contentPanel = new JPanel(); // TODO: make it scrollable
       //contentPanel.setLayout(new MigLayout("insets 12, novisualpadding, hidemode 3, fill, gap 6", "[]12[][grow]", "")); // layout, column, row
       contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
@@ -115,41 +120,43 @@ public class ChannelTabPanel extends AbstractChannelTabPanel { // extends JPanel
             new Font("Tahoma", 1, 11)
          )
       );
+      */
+
+
 
       /**
       * Add our components to the base container
       */
-      
-      contentPanel.add(channelLabel);
+
+      //contentPanel.add(channelLabel);
       //contentPanel.add(channelTextArea);
 
-      
-      
+
       // GRAPH
+      /*
       // create a JGraphT graph
       ListenableGraph<String, DefaultEdge> g =
           new DefaultListenableGraph<>(new DefaultDirectedGraph<>(DefaultEdge.class));
-      
+
       //Set<DefaultEdge> edges = g.edgeSet();
 
       // create a visualization using JGraph, via an adapter
       JGraphXAdapter<String, DefaultEdge> jgxAdapter = new JGraphXAdapter<>(g);
-      
+
       //Dimension DEFAULT_SIZE = new Dimension(530, 320);
 
       //applet.setPreferredSize(DEFAULT_SIZE);
       mxGraphComponent component = new mxGraphComponent(jgxAdapter); // this is a scroll pane!
-      
-      
-      JLabel lblNewLabel = new JLabel("New label");
+
+
+      JLabel lblNewLabel = new JLabel("Channel diagram");
       component.setColumnHeaderView(lblNewLabel);
-	
+
       // this shows the background color!
       component.getViewport().setOpaque(true);
-      component.getViewport().setBackground(Color.YELLOW);
-		
+      component.getViewport().setBackground(Color.WHITE);
       component.setConnectable(false);
-      
+
       // DATA
       String v1 = "Trasnformer 1\nGet x and put y";
       String v2 = "Transformer 2\nGet z and put w";
@@ -165,60 +172,108 @@ public class ChannelTabPanel extends AbstractChannelTabPanel { // extends JPanel
       g.addEdge(v1, v2, new MyEdge());
       g.addEdge(v2, v3, new MyEdge());
       g.addEdge(v3, v4, new MyEdge());
-      
-      
+
+
       String v5 = "Filter 1";
       String v6 = "Filter 2";
-      
+
       g.addVertex(v5);
       g.addVertex(v6);
-      
+
       g.addEdge(v5, v6, new MyEdge());
       // /DATA
-      
+
       mxStackLayout layout = new mxStackLayout(jgxAdapter, true, 50, 10, 10, 0);
       layout.execute(jgxAdapter.getDefaultParent());
-      
+
+      */
+
       // /GRAPH
-      
+
+
+      /* <<<<<<<<<<<<<
       contentPanel.add(component);
-      
-      
+
+
       JScrollPane scrollPane = new JScrollPane(contentPanel);
       scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
       scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
       //scrollPane.setBounds(50, 30, 300, 50); // TODO: the scroll should be of the size of the container panel
-      
-      
+      */
+
+
 
       /**
       * Add base container to Mirth's more base-er container
       */
-      add(scrollPane, "grow");
+      //add(scrollPane, "grow");
+      //add(component);
    }
 
    @Override
-   public void load(Channel channel) {
+   public void load(Channel channel)
+   {
       System.out.println("Hello from the CLIENT ChannelTabPanel.load()");
 
-      String text = "";
-
       Connector source = channel.getSourceConnector();
+
+      List<String> nodes = new ArrayList<>();
+
+
+      // GRAPH
+      // create a JGraphT graph
+      ListenableGraph<String, DefaultEdge> g =
+          new DefaultListenableGraph<>(new DefaultDirectedGraph<>(DefaultEdge.class));
+
+      // create a visualization using JGraph, via an adapter
+      JGraphXAdapter<String, DefaultEdge> jgxAdapter = new JGraphXAdapter<>(g);
+
+      mxGraphComponent component = new mxGraphComponent(jgxAdapter); // this is a scroll pane!
+
+      //component.setPreferredSize(new Dimension(530, 320)); // doesn't show the scrollbars
+      component.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+      component.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+      System.out.println(component.getViewport().toString());
+      System.out.println(getWidth());
+      System.out.println(getHeight());
+
+      // this shows the background color!
+      component.getViewport().setOpaque(true);
+      component.getViewport().setBackground(Color.WHITE);
+      component.setConnectable(false);
 
       //   for (Rule rule: source.getFilter().getElements())
       //   {
       //     text += "(F)"+ rule.getName() + " " + rule.getOperator() + " -> ";
       //   }
 
+      String node;
+      String next;
+
       for (Step step: source.getTransformer().getElements())
       {
-         text += "(T) "+ step.getName() + " -> \n";
+         node = "(T) "+ step.getName();
+         nodes.add(node);
+         g.addVertex(node);
       }
+
+      // add edges, does not process the last one since the last won't have an edge
+      for (int i=0; i<nodes.size()-1; i++)
+      {
+         node = nodes.get(i);
+         next = nodes.get(i+1);
+         g.addEdge(node, next, new MyEdge());
+      }
+
+
+      mxStackLayout layout = new mxStackLayout(jgxAdapter, false, 50, 10, 10, 0);
+      layout.execute(jgxAdapter.getDefaultParent());
 
       //   List<Connector> destinations = channel.getDestinationConnectors();
       // TODO:
 
-      channelTextArea.setText(text);
+      add(component);
    }
 
    @Override
